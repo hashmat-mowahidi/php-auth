5dde5e2600e81dbce6f4a343ebcd7b4803b4fe2a
hashmat <hashmat.mowahidi@gmail.com>
2026-01-02 22:09:04 +0100

    Logout is completed

--- includes/login_view.inc.php
+++ includes/login_view.inc.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+function output_username()
+{
+    if (isset($_SESSION['user_id'])) {
+        echo 'You are logged in as ' . $_SESSION['username'];
+    } else {
+        echo "You are not logged in";
+    }
+}
+
 function check_login_errors()
 {
     if (isset($_SESSION["errors_login"])) {
@@ -13,8 +22,7 @@ function check_login_errors()
             echo '<p class="error">' . $error . '</p>';
         }
         unset($_SESSION["errors_login"]);
-    }
-    else if(isset($_GET["login"]) && $_GET["login"] === "success") {
+    } else if (isset($_GET["login"]) && $_GET["login"] === "success") {
         echo '<p class="result">Login Success!</p>';
     }
 }
--- index.php
+++ index.php
@@ -18,25 +18,56 @@ require_once 'includes/login_view.inc.php';
 </header>
 
 <body>
-    <h3>Login</h3>
-    <form action="includes/login.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="u_password" placeholder="Password">
-        <button type="submit">Login</button>
-    </form>
+
+    <h3>
+        <?php
+        output_username();
+        ?>
+    </h3>
+
     <?php
-        check_login_errors();
+    if (!isset($_SESSION["user_id"])) {
+    ?>
+        <h3>Login</h3>
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="u_password" placeholder="Password">
+            <button type="submit">Login</button>
+        </form>
+    <?php
+    }
+    ?>
+    <?php
+    check_login_errors();
     ?>
 
-    <h3>Signup</h3>
-    <form action="includes/signup.inc.php" method="post">
-        <?php
-        signup_inputs();
-        ?>
-        <button type="submit">Signup</button>
-    </form>
+    <?php
+    if (!isset($_SESSION["user_id"])) {
+    ?>
+        <h3>Signup</h3>
+        <form action="includes/signup.inc.php" method="post">
+            <?php
+            signup_inputs();
+            ?>
+            <button type="submit">Signup</button>
+        </form>
+
+    <?php
+    }
+    ?>
+
     <?php
     check_signup_errors();
+    ?>
+
+    <?php
+    if (isset($_SESSION["user_id"])) {
+    ?>
+        <form action="includes/logout.inc.php" method="post">
+            <button type="submit">Logout</button>
+        </form>
+    <?php
+    }
     ?>
 </body>
 
